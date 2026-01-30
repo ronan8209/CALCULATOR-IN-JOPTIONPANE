3eae15d5d843f845de2ae2f5f97d571bb5c0b8aa
ronan8209 <newrobloxrobux23@gmail.com>
2026-01-30 18:27:43 +0800

    Add files via upload

---
+++ calc.java
@@ -0,0 +1,49 @@
+import javax.swing.JOptionPane;
+public class calc {
+    public static void main(String[] args) {
+        do {
+            try {
+                String num1 = JOptionPane.showInputDialog(null, "enter first number", "calculator", JOptionPane.OK_CANCEL_OPTION);
+                if (num1 == null) {
+                    JOptionPane.showMessageDialog(null, "exting....", "calculator", JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                }
+                String num2 = JOptionPane.showInputDialog(null, "enter second number", "calculator", JOptionPane.OK_CANCEL_OPTION);
+                if (num2 == null) {
+                    JOptionPane.showMessageDialog(null, "exting....", "calculator", JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                }
+                String op = JOptionPane.showInputDialog(null, "enter operator(+,-,*,/)", "calculator", JOptionPane.OK_CANCEL_OPTION);
+                if (op == null) {
+                    JOptionPane.showMessageDialog(null, "exting....", "calculator", JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                }
+                double n1 = Double.parseDouble(num1);
+                double n2 = Double.parseDouble(num2);
+                double result;
+
+                switch (op) {
+                    case "+":
+                        result = n1 + n2;
+                        break;
+                    case "-":
+                        result = n1 - n2;
+                        break;
+                    case "*":
+                        result = n1 * n2;
+                        break;
+                    case "/":
+                        result = n1 / n2;
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "invalid operator", "calculator", JOptionPane.ERROR_MESSAGE);
+                        continue;
+                }
+                JOptionPane.showMessageDialog(null, "Result: " + result);
+            } catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(null,"invalid type should be a number","calc",JOptionPane.ERROR_MESSAGE);
+            }
+        } while (true);
+    }
+}
+ 
